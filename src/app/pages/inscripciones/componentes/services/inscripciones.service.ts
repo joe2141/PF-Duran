5521f4811855a9c250ecf7a1a7e30607d4f2eee0
joe2141 <elsuperjoy@gmail.com>
2023-05-21 14:24:38 -0600

    se consume la info desde una base de datos

--- src/app/pages/inscripciones/componentes/services/inscripciones.service.ts
+++ src/app/pages/inscripciones/componentes/services/inscripciones.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Inscripciones } from '../models/index';
 import { BehaviorSubject, Observable, map, } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { enviroment } from '../../../../../environments/environments';
 
 @Injectable({
     providedIn: 'root'
@@ -8,105 +10,46 @@ import { BehaviorSubject, Observable, map, } from 'rxjs';
 
 export class InscripcionesService {
 
-    private inscripciones$ = new BehaviorSubject<Inscripciones[]>([
-        {
-            id: 1,
-            alumnoId: 1,
-            cursoId: 1,
-            alumno: 'PAULINO',
-            curso: 'Angular',
-            fecha_inicio: new Date(),
-        },
-        {
-            id: 2,
-            alumnoId: 1,
-            cursoId: 2,
-            alumno: 'PAULINO',
-            curso: 'React',
-            fecha_inicio: new Date(),
-        },
-        {
-            id: 3,
-            alumnoId: 1,
-            cursoId: 3,
-            alumno: 'PAULINO',
-            curso: 'Asado Profecional',
-            fecha_inicio: new Date(),
-        },
-        {
-            id: 4,
-            alumnoId: 2,
-            cursoId: 1,
-            alumno: 'ANNA',
-            curso: 'Angular',
-            fecha_inicio: new Date(),
-        },
-        {
-            id: 5,
-            alumnoId: 2,
-            cursoId: 2,
-            alumno: 'ANNA',
-            curso: 'React',
-            fecha_inicio: new Date(),
-        },
-        {
-            id: 6,
-            alumnoId: 3,
-            cursoId: 3,
-            alumno: 'RAMIRO',
-            curso: 'Asado Profecional',
-            fecha_inicio: new Date(),
-        },
-        {
-            id: 7,
-            alumnoId: 4,
-            cursoId: 1,
-            alumno: 'PAMELA',
-            curso: 'Angular',
-            fecha_inicio: new Date(),
-          },
-          {
-            id: 8,
-            alumnoId: 5,
-            cursoId: 1,
-            alumno: 'MARTHA',
-            curso: 'Angular',
-            fecha_inicio: new Date(),
-          },
-          {
-            id: 9,
-            alumnoId: 6,
-            cursoId: 3,
-            alumno: 'MIREIA',
-            curso: 'Asado Profecional',
-            fecha_inicio: new Date(),
-          },
-    ])
+  private inscripciones$ = new BehaviorSubject<Inscripciones[]>([]);
 
-    constructor() { }
+  constructor(private http: HttpClient) {
+    this.fetchInscripciones(); // Llamar al método para obtener las inscripciones al iniciar el servicio
+  }
 
-    obtenerInscripciones(): Observable<Inscripciones[]> {
-        return this.inscripciones$.asObservable();
-    }
+  obtenerInscripciones(): Observable<Inscripciones[]> {
+    return this.inscripciones$.asObservable();
+  }
 
-    obtenerInscripcionesPorId(id: number): Observable<Inscripciones | undefined> {
-        return this.inscripciones$.asObservable()
-            .pipe(
-                map((inscripciones) => inscripciones.find((a) => a.id === id))
-            )
-    }
+  obtenerInscripcionesPorId(id: number): Observable<Inscripciones | undefined> {
+    return this.inscripciones$.asObservable()
+      .pipe(
+        map((inscripciones) => inscripciones.find((a) => a.id === id))
+      );
+  }
 
-    obtenerInscripcionesPorAlumnoId(id: number): Observable<Inscripciones | undefined> {
-        return this.inscripciones$.asObservable()
-            .pipe(
-                map((inscripciones) => inscripciones.find((a) => a.alumnoId === id))
-            )
-    }
+  obtenerInscripcionesPorAlumnoId(id: number): Observable<Inscripciones[]> {
+    return this.inscripciones$.asObservable()
+      .pipe(
+        map((inscripciones) => inscripciones.filter((a) => a.alumnoId === id))
+      );
+  }
 
-    obtenerInscripcionesPorCursoId(id: number): Observable<Inscripciones | undefined> {
-        return this.inscripciones$.asObservable()
-            .pipe(
-                map((inscripciones) => inscripciones.find((a) => a.cursoId === id))
-            )
-    }
+  obtenerInscripcionesPorCursoId(id: number): Observable<Inscripciones[]> {
+    return this.inscripciones$.asObservable()
+      .pipe(
+        map((inscripciones) => inscripciones.filter((a) => a.cursoId === id))
+      );
+  }
+
+  private fetchInscripciones() {
+    this.http.get<Inscripciones[]>(`${enviroment.apiBaseUrl}/inscripciones`)
+      .subscribe(
+        (inscripciones) => {
+          this.inscripciones$.next(inscripciones);
+        },
+        (error) => {
+          console.log('Error al obtener las inscripciones:', error);
+        }
+      );
+  }
 }
