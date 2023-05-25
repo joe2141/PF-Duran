72ffb8682185ad80eea401b76cb747044037ea29
joe2141 <elsuperjoy@gmail.com>
2023-05-24 20:45:55 -0600

    eliminacion desde detalles

--- src/app/pages/alumnos/detalles-alumnos/detalles-alumnos.component.ts
+++ src/app/pages/alumnos/detalles-alumnos/detalles-alumnos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlumnosService } from '../componentes/services/alumnos.service';
 import { Alumno } from '../componentes/models/index';
@@ -13,41 +13,56 @@ import { Subject, takeUntil } from 'rxjs';
   templateUrl: './detalles-alumnos.component.html',
   styleUrls: ['./detalles-alumnos.component.scss']
 })
-export class DetallesAlumnosComponent implements OnDestroy {
-
+export class DetallesAlumnosComponent implements OnInit, OnDestroy {
   alumno: Alumno | undefined;
-  private destroyed$ = new Subject()
+  inscripciones: Inscripciones[] | undefined;
+
+  private destroyed$ = new Subject();
   dataSource = new MatTableDataSource<Inscripciones>();
 
-  displayedColumns: string[] = ['id', 'curso', 'fecha_inscripcion', 'desuscribir']
+  displayedColumns: string[] = ['id', 'curso', 'fecha_inscripcion', 'desuscribir'];
 
   aplicarFiltros(ev: Event): void {
     const inputValue = (ev.target as HTMLInputElement)?.value;
     this.dataSource.filter = inputValue?.trim()?.toLowerCase();
   }
 
-  constructor(private matDialog: MatDialog,
+  constructor(
+    private matDialog: MatDialog,
     private activatedRoute: ActivatedRoute,
     private inscripcionesService: InscripcionesService,
     private alumnosServices: AlumnosService,
-  ) {
-    this.inscripcionesService.obtenerInscripciones()
+  ) {}
+
+  ngOnInit(): void {
+    this.inscripcionesService
+      .obtenerInscripciones()
       .subscribe((inscripciones) => {
-        this.dataSource.data = inscripciones.filter(x => x.alumnoId === parseInt(this.activatedRoute.snapshot.params['id']));
+        this.dataSource.data = inscripciones.filter(
+          (x) => x.alumnoId === parseInt(this.activatedRoute.snapshot.params['id'])
+        );
       });
-    this.alumnosServices.obtenerAlumnoPorId(parseInt(this.activatedRoute.snapshot.params['id']))
+
+    this.alumnosServices
+      .obtenerAlumnoPorId(parseInt(this.activatedRoute.snapshot.params['id']))
       .pipe(takeUntil(this.destroyed$))
-      .subscribe((alumno) => this.alumno = alumno);
+      .subscribe((alumno) => (this.alumno = alumno));
   }
 
-  desuscribirAlumno(alumnoForDelete: Alumno): void {
-    if (confirm("Esta seguro de borrar?")) {
-      this.dataSource.data = this.dataSource.data.filter(
-        (alumnoActual) => alumnoActual.id !== alumnoForDelete.id,
-      );
+  eliminarInscripcion(inscripcion: Inscripciones): void {
+    if (!confirm('¿Estás seguro de que deseas eliminar esta inscripción?')) {
+      return;
     }
+
+    this.inscripcionesService
+      .eliminarInscripcion(inscripcion.id)
+      .subscribe(() => {
+        this.dataSource.data = this.dataSource.data.filter((x) => x.id !== inscripcion.id);
+      });
   }
+
   ngOnDestroy(): void {
-    this.destroyed$.next(true)
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
